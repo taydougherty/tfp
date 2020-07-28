6465fa7063dcc41b2b9988d804ee308a05c3e5e5
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-07-28 10:52:23 -0700

    updated dependencies and login route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/admin/Navbar";
+import Login from "./components/admin/Login";
 import Footer from "./components/admin/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Login />
       <Footer />
     </div>
   );
