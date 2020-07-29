8cb8dff9a64826f580aa650e78047405cfb835a4
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-07-28 17:16:04 -0700

    added routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Navigation from "./components/admin/Navbar";
-// import Login from "./components/admin/Login";
+import Login from "./components/admin/Login";
 import Register from "./components/admin/Register";
 import Footer from "./components/admin/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      {/* <Login /> */}
-      <Register />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
