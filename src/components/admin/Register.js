a302f4d6617957b984454cdca148f34cb67af53b
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-07-28 17:00:39 -0700

    removed auth route for now

--- src/components/admin/Register.js
+++ src/components/admin/Register.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
-import Auth from "./utils/Auth";
 import Navigation from "./Navbar";
 import "./Register.scss";
 
