3a3ef30f27504a8b3fcd6e86880c072d738c4fc8
Dangggchris <christophermtdang@gmail.com>
2020-07-31 16:22:17 -0700

    updated component paths

---
+++ src/pages/public/Leadership/Leadership.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import NavBar from "../../../Components/public/NavBar";
+import Footer from '../../../Components/admin/Footer/Footer'
+
+class Leadership extends Component {
+
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="jumbotron bg-white">
+                                <h1>Leadership</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row h-75 cards">
+                        <div className="col-6">
+                            <div class="card border-light">
+                                <div class="card-body d-flex flex-column">
+                                  <img src="https://picsum.photos/seed/picsum/200/300" alt="placeholder"></img>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                          <div class="card border-light">
+                                <div class="card-body d-flex flex-column">
+                                  <h5 class="card-title">Sheila Mojtehedi, CEO</h5>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <div class="card-body d-flex flex-column">
+                                  <h5>Notable Wins</h5>
+                                  <ul>
+                                    <li>Winner</li>
+                                    <li>Winner</li>
+                                    <li>Chicken Dinner</li>
+                                  </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div >
+        )
+    }
+}
+
+export default Leadership;
