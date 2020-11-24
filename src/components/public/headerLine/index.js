651772a04020b510ae5b6fa2cbb00a0514161fcd
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-11-24 14:39:50 -0800

    updated css

---
+++ src/components/public/headerLine/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+const HeaderLine = () => {
+  return (
+    <div>
+      <hr className="headerLine" />
+    </div>
+  );
+};
+
+export default HeaderLine;
