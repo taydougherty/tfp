c0fbebd5363954cd7e6e2199a957747b25a2e2e6
Cedric Orejola <cedricorejola@gmail.com>
2020-12-02 11:28:28 -0800

    added css cursor property to hidden text

--- src/components/admin/PreviousBlogPost/PrevBlogEntry/PrevBlogEntry.js
+++ src/components/admin/PreviousBlogPost/PrevBlogEntry/PrevBlogEntry.js
@@ -23,13 +23,12 @@ function PrevBlogEntry(props) {
             <tr key={props.key}>
                 <td > {moment(props.date).format("MMM Do YYYY")} </td>
                 <td className="card-title" defaultValue={props.title}>{props.title}</td>
-                <td className="card-text" defaultValue={props.body}><div onClick={onToggle} className={showText ? null : "line-clamp"}> {props.body}</div></td>
+                <td className="card-text" defaultValue={props.body}><div onClick={onToggle} className={showText ? "full-text" : "line-clamp"}> {props.body}</div></td>
                 <td className="card-text" defaultValue="N/A">{props.image}</td>
                 <td ><button className="blogDelete" value={props.id} type="submit" onClick={handleDelete}>DELETE</button></td>
             </tr>
         </tbody>
     )
-
 }
 
 
